071c67d0ac73a167f060b4c7b66031d19824937a
Sokhna Fall <84427104+sokhnarfall@users.noreply.github.com>
2023-03-22 18:36:58 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const http = require('http');
 const fs = require('fs');
 
 // Require minimist module (make sure you install this one via npm).
-// const minimist = require('minimist');
+const minimist = require('minimist');
 
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
 var argv = require('minimist')(process.argv.slice(2));
